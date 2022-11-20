f04550aa95f20c8e76abf0c9e6786607f5aae7ca
LYS312 <lyssilis312@gmail.com>
2022-11-20 16:26:21 +0000

    enum datatatype in Vote model

--- App/models/vote.py
+++ App/models/vote.py
@@ -3,14 +3,14 @@ from datetime import datetime
 from enum import Enum
 
 class Value (Enum):
-    upvote= "Upvote"
-    downvote="Downvote"
+    UPVOTE= 1
+    DOWNVOTE= -1
 
 class Vote (db.Model):
     id= db.Column(db.Integer, primary_key=True)
     staff_id = db.Column(db.Integer, db.ForeignKey("staff.id"), nullable=False)
     review_id = db.Column(db.Integer, db.ForeignKey("staff.id"), nullable=False)
     time= db.Column(db.DateTime, default=datetime.utcnow)
-    value= db.Column(db.String(10), nullable=False)
+    value= db.Column(Enum(Value), nullable=False)
 
     

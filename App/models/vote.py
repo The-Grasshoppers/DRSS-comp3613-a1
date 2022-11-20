84694b9956486859b87bd941a2694cf309fe3691
LYS312 <lyssilis312@gmail.com>
2022-11-20 04:30:46 +0000

    vote model created

--- App/models/vote.py
+++ App/models/vote.py
@@ -1,8 +1,16 @@
 from App.database import db
 from datetime import datetime
+from enum import Enum
+
+class Value (Enum):
+    upvote= "Upvote"
+    downvote="Downvote"
 
 class Vote (db.Model):
     id= db.Column(db.Integer, primary_key=True)
     staff_id = db.Column(db.Integer, db.ForeignKey("staff.id"), nullable=False)
     review_id = db.Column(db.Integer, db.ForeignKey("staff.id"), nullable=False)
     time= db.Column(db.DateTime, default=datetime.utcnow)
+    value= db.Column(db.String(10), nullable=False)
+
+    

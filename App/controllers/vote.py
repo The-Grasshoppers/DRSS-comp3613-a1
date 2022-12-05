3d5ea13007c08262c4df826add2ffc85d10822e2
joshuapj <joshpersad180@gmail.com>
2022-12-05 15:18:14 +0000

    changed vote controllers to use filter_by

--- App/controllers/vote.py
+++ App/controllers/vote.py
@@ -4,21 +4,21 @@ from App.models.vote import Value
 
 # Get all votes for a review, given the review id
 def get_votes(review_id):
-    votes = Vote.query.get(review_id)
+    votes = Vote.query.filter_by(review_id=review_id)
     if not votes:
         return None
     return votes
 
 # Get all votes of a staff member
 def get_staff_votes(staff_id):
-    votes = Vote.query.get(staff_id)
+    votes = Vote.query.filter_by(staff_id=staff_id)
     if not votes:
         return None
     return votes
 
 # Get staff member's vote from a review
 def get_vote_by_staff(review_id, staff_id):
-    votes = Vote.query.get(review_id)
+    votes = Vote.query.filter_by(review_id=review_id)
     if not votes:
         return None
 
